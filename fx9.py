cf8cfb47321254f92f3187e252b8e8a314546b86
Fanan-2023 <122181416+Fanan-2023@users.noreply.github.com>
2023-01-07 22:27:26 +0100

    Add files via upload

---
+++ fx9.py
@@ -0,0 +1,30 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+def homescreen () :
+    os.system ('clear')
+    print ("          .-;;-. ")
+    print ("         / .--. \  ")
+    print ("        / /    \ \  ")
+    print ("        | |    | |  ")
+    print ("        | |.-""-.|  ")
+    print ("       ///`.::::.`\   ") 
+    print ("      ||| ::/  \ ::;  ")
+    print ("      ||; ::\__/ ::;  ")
+    print ("        \ \.:::::: /  ")
+    print ("        `=`:-..-``  ")
+    print ('  script  فدﺤ مﺗ')
+    print (' ')
+    V = input ("\033[1;36m  This is a VIP script that does not work in all phones \033[1;32m ==> ")
+    if V == 'my vip ' :
+        os.system ('You only have a VIP')
+    else:
+        print ('errors ')
+        timeout(0.6)
+        homescreen ()
+
+homescreen ()
