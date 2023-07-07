07c1a09e802098736f8fdcc46c8381a4e3a6ad72
Fanan-2023 <122181416+Fanan-2023@users.noreply.github.com>
2023-07-07 13:01:44 +0100

    Add files via upload

---
+++ amine.py
@@ -0,0 +1,30 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+def homescreen () :
+    os.system ('clear')
+    print ("          .-;;-. ")
+    print ("         / .--. \  ")
+    print ("        / /    \ \  ")
+    print ("        | |    | |  ")
+    print ("        | |.-""-.|  ")
+    print ("       ///`.::::.`\   ") 
+    print ("      ||| ::/  \ ::;  ")
+    print ("      ||; ::\__/ ::;  ")
+    print ("        \ \.:::::: /  ")
+    print ("        `=`:-..-``  ")
+    print ('💀💀')
+    print (' ')
+    V = input ("\033[1;36m  password  \033[1;32m ==> ")
+    if V == 'Amine Makroud' :
+        os.system ('python hack1.py')
+    else:
+        print ('errors ')
+        timeout(0.6)
+        homescreen ()
+
+homescreen ()
