bfa098aad9288f576d7f3ba6c80bac584dc86b45
Fanan-2023 <122181416+Fanan-2023@users.noreply.github.com>
2023-07-10 10:23:27 +0100

    Add files via upload

---
+++ f.py
@@ -0,0 +1,9 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+print ('This is a script made for Android 10 and below')
+os.system ('figlet')
