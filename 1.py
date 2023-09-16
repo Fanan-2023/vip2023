9e204606a4ee7a2db466089cd07bbc0087880ccc
Fanan-2023 <122181416+Fanan-2023@users.noreply.github.com>
2023-09-16 13:45:01 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,36 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+def main (main):
+    print ('')
+    time.sleep(0.2)
+    print ("\033[1;36m...ﺮﻈﺘﻧﺍ")
+    time.sleep(4)
+    print ('\033[1;31m')
+    print ("")
+    print ("")
+    print ("")
+    os.system ('clear')
+    print ("███████╗██████╗░███████╗███████╗░░░░░███████╗██╗██████╗░███████╗")
+    print ("██╔════╝██╔══██╗██╔════╝██╔════╝░░░░░██╔════╝██║██╔══██╗██╔════╝")
+    print ("█████╗░░██████╔╝█████╗░░█████╗░░░░░░░█████╗░░██║██████╔╝█████╗░░")
+    print ("██╔══╝░░██╔══██╗██╔══╝░░██╔══╝░░░░░░░██╔══╝░░██║██╔══██╗██╔══╝░░")
+    print ("██║░░░░░██║░░██║███████╗███████╗░░░░░██║░░░░░██║██║░░██║███████╗")
+    print ("╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚══════╝░░░░░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚══════╝")
+    time.sleep(0.2)
+    print ("\033[1;36m")
+    print ("")
+    print ("‎")
+    print ("")
+    print ("")
+    fx9 = input (" \033[1;33m ﺔﯾﺤﺿ يﺬﯾأ  ==> \033[1;32m")
+    fx9 = input (" \033[1;33m Do you want to continue y / n  ==> \033[1;32m")
+    time.sleep(0.2)
+    if fx9 == 'y' :
+         os.system ('python root2.py')
+main (main)
+home_main ()
---
+++ 4.py
@@ -0,0 +1,34 @@
+from time import sleep as timeout
+import time
+import base64
+import socket
+import sys
+import os
+os.system ('clear')
+def main (main):
+    print ('')
+    time.sleep(0.2)
+    print ("\033[1;36m...ﺮﻈﺘﻧﺍ")
+    time.sleep(4)
+    print ('\033[1;31m')
+    print ("")
+    print ("")
+    print ("")
+    os.system ('clear')
+    os.system ('filget WHATSAPP')
+    time.sleep(0.2)
+    print ("\033[1;36m    تﺑﯾرڪﺴ ءارﺴٌُﻟ")
+    print ("   ﻟﻤﮑﺗﻟ بﺎﺴﺗو يﻓ توﺑ مﻗر ﻰﻠﻋ جﻧروأ مھرد 100 ﻟﺴرأ")
+    print ("‎ +212644848897 :مﻗر")
+    os.system('xdg-open https://api.whatsapp.com/send?phone=‎+212644848897')
+    print ("")
+    print ("")
+    fx9 = input (" \033[1;33m ﺔﯾﺤﺿ بﺎﺴﺤ مﻗر  ==> \033[1;32m")
+    time.sleep(0.2)
+    if fx9 == 'my vip ' :
+         os.system ('You only have a VIP')
+    else:
+         print ('      😶ضﻋﺑ📄تﺑﯾرڪﺴ💶 يرﺗﺴِّْﺗ مﻟ ')
+         os.system('xdg-open https://api.whatsapp.com/send?phone=‎+212644848897')
+main (main)
+home_main ()
